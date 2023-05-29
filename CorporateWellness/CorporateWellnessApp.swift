053b4e838f0c7f8cbbe0eabca374356085069376
NasiruOjochide <nasiruojochide@gmail.com>
2023-05-29 12:58:29 +0100

    Initial Commit

---
+++ CorporateWellness/CorporateWellnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  CorporateWellnessApp.swift
+//  CorporateWellness
+//
+//  Created by Danjuma Nasiru on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CorporateWellnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
