d5ecaa8b4a73ac208a0852b3cf716437a0ec003d
NasiruOjochide <nasiruojochide@gmail.com>
2023-05-31 09:48:12 +0100

    Initial Commit

---
+++ DataBest/DataBestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataBestApp.swift
+//  DataBest
+//
+//  Created by Danjuma Nasiru on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DataBestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
